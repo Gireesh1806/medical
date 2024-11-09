839ba91162d4d0f73bb7b0b7bb552a47565a03f7
Gireesh1806 <124286012+Gireesh1806@users.noreply.github.com>
2024-11-09 23:00:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = 'your_secret_key'
+    """ hello this is configuration file"""
     
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
