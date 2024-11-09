88f426ffad746db3f0f1c51a2b506d0360371264
Gireesh1806 <124286012+Gireesh1806@users.noreply.github.com>
2024-11-08 22:55:35 -0600

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'  
-    '''SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")'''
+    
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
